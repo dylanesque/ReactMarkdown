66ab4895674970e95aca053ced19b398af4b4dad
Michael Caveney <dylanesque27@gmail.com>
2018-09-06 14:22:12 -0400

    Implemented basic style and Header & TextInput components

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+  <div className="heading">
+    <h1>This is my Markdown previewer!</h1>
+  </div>
+  );
+};
+
+export default Header;
--- tasks.js
+++ tasks.js
@@ -1,5 +1,5 @@
-//  I can see a textarea element with a corresponding id="editor"
-//   I can see an element with a corresponding id="preview"
+
+// I can see an element with a corresponding id="preview"
 // When I enter text into the #editor element, the #preview element is updated as I type to display the content of the textarea.
 //  When I enter GitHub flavored markdown into the #editor element, the text is rendered as HTML in the #preview element as I type (HINT: You don't need to parse Markdown yourself - you can import the Marked library for this: https://cdnjs.com/libraries/marked).
 // When my markdown previewer first loads, the default text in the #editor field should contain valid markdown that represents at least one of each of the following elements: a header (H1 size), a sub header (H2 size), a link, inline code, a code block, a list item, a blockquote, an image, and bolded text.
