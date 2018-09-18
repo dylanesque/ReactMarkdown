c315fd89ffe5c9f0e3f57fcb227e31981152cb3d
Michael Caveney <dylanesque27@gmail.com>
2018-09-18 11:23:17 -0400

    Refactored component and implemented data binding

--- src/App.js
+++ src/App.js
@@ -1,15 +1,53 @@
 import React, { Component } from 'react';
 
-import TextInput from './TextInput';
-import Header from './Header';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      value: 'Here is my placeholder value'
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('Text was submitted to be previewed ' + this.state.value);
+    event.preventDefault();
+  }
+
   render() {
     return (
       <div className="App">
-        <Header />
-        <TextInput />
+
+        <div className="heading">
+          <h1>This is my Markdown previewer!</h1>
+        </div>
+
+        <div>
+      <h2>Enter some markdown to double-check your output!</h2>
+
+      <form onSubmit={ this.handleSubmit }>
+      <textarea
+        value={this.state.value}
+        onChange={this.handleChange}
+        id="editor"
+        rows="10"
+        cols="80" />
+        <br/>
+        <input type="submit" value="Submit" />
+        </form>
+      </div>
+
+      <div id="preview">{ this.state.value }</div>
+
       </div>
     );
   }
