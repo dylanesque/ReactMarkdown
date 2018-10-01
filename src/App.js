a79de392d11602fc72a097a47d9c233e7571ca0c
Michael Caveney <dylanesque27@gmail.com>
2018-10-01 16:08:01 -0400

    Successfully implemented marked.js middleware

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 
+import marked from 'marked';
+
 import './App.css';
+
+const renderer = new marked.Renderer();
 
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      value: 'Here is my placeholder value'
+      value: placeholderText
     }
 
     this.handleChange = this.handleChange.bind(this);
@@ -20,29 +24,28 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-
         <div className="heading">
           <h1>This is my Markdown previewer!</h1>
+          <h2>Enter some markdown to double-check your output!</h2>
         </div>
-
-        <div>
-      <h2>Enter some markdown to double-check your output!</h2>
-
-      <form>
-      <textarea
-        value={this.state.value}
-        onChange={this.handleChange}
-        id="editor"
-        rows="10"
-        cols="86" />
+        <form>
+          <textarea
+            value={this.state.value}
+            onChange={this.handleChange}
+            id="editor"
+            rows="10"
+            cols="86" />
         </form>
-      </div>
-
-      <div id="preview">{ this.state.value }</div>
-
+        <Preview value={this.state.value} />
       </div>
     );
   }
+};
+
+const Preview = props => {
+  return (
+      <div id='preview' dangerouslySetInnerHTML={{__html: marked(props.value, { renderer: renderer })}} />
+    );
 }
 
 const placeholderText = `
@@ -50,7 +53,7 @@ const placeholderText = `
   ## Enter some Markdown in this box, and see the output rendered below.
 
   > Don't know what Markdown is? No problem! It's a templating language
-  > designed to quickly draft HTML! 
+  > designed to quickly draft HTML!
 
   For a syntax refresher, visit https://guides.github.com/features/mastering-markdown/
 
